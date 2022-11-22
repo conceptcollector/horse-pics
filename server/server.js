1925f85fe140e33abe524b4143a1c86e1dccc052
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-11-21 22:21:35 -0600

    Added a comment

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+const sessionMiddleware = require('./modules/session-middleware');
+const passport = require('./strategies/user.strategy');
+
+// Route includes
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport Session Configuration //
+app.use(sessionMiddleware);
+
+// start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Routes */
+
+// Serve static files
+app.use(express.static('build'));
+
+// App Set //
+const PORT = process.env.PORT || 5000;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
---
+++ src/HorsePic.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HorsePic() {
+    // What in the actual fuck??
+}
