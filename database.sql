06a079bdfeb491ba28bb789995ff0e05e4be0ee0
Christopher Boyd <boyd.christopher.t@gmail.com>
2023-10-17 17:52:51 -0500

    Made comments for all the noteworthy bits of Roundtable episode 1

--- database.sql
+++ database.sql
@@ -44,4 +44,52 @@ CREATE TABLE index (
 INSERT INTO index (topic, podcast, episode, comments)
 VALUES
 ('Search and Destroy', 'The Roundtable of Gentlemen', 'Episode 1: It Begins',
-'Intro song. Performed by The Stooges. David Bowie mix.')
+'Intro song. Performed by The Stooges. David Bowie mix.'),
+-- "Hey", first word of Roundtable
+-- "Hi", first word by a distinct voice, that of Holden McNeely...
+-- Ben fucks up the intro
+-- Kevin Barnett
+-- Holden McNeely
+-- Ed Larson
+-- Cupcake
+-- Ben Kissel
+-- Gary Coleman's death
+-- Shannon Price (Gary Coleman's wife)
+-- feeder/gainer
+-- cake farts
+-- dolphins in (2010) oil spill
+-- Griswolds, Vacation
+-- ghosts (specifically Gary Coleman)
+-- Charlton Heston('s ghost)
+-- Ed suggests a $20,000 reward to kill Shannon Price
+-- Ted Haggart's new church
+-- Rudy Giuliani
+-- synthetic DNA
+-- Islamic mosque
+-- Jason Voohees
+-- Ben's from Wisconsin
+-- Wisconsin man is stealing dirty diapers
+-- Texas? That man would have been dead (per Cupcake)
+-- NY cops should no longer shoot to kill
+-- Band of Brothers
+-- 3 more deaths in Germany from WWII bomb
+-- 24:46 into episode 1, Holden calls "Cupcake" Marcus
+-- Ben's Opa was a Nazi Naval Commander
+-- Ben's Vater fled because he didn't want to be the son of a Nazi
+-- Ben's the grandson of a Nazi
+-- Holden says "Mommy and Daddy loved me."
+-- Holden's grandpa was a Golden Gloves champion
+-- Hee Haw
+-- Ben's grandpa was a huge political figure, started a labor union. Said the camps were terrible, though he didn't know about them. "Obviously, he was antisemetic." Also said that Germany almost took over the world and is the size of Wisconsin!
+-- Pope was a Nazi ("Joey Ratz" - Marcus)
+-- neonazis are coming back (2010)
+-- Tea Party
+-- Jackie Knotts (called Obama and his opponent ragheads)
+-- Islamic mosque by WTC hole
+-- Hell's Angels
+-- Bum found plans to WTC plans
+-- UHF
+-- Superman
+-- male pornstar got fired, killed someone, injured 3 others
+-- Former podcast (Ben says "we used to..." (Holden?)) The Brain and the Beast
+-- Marcus's boiler room is suggested as a location to murder Ed/have an orgy
